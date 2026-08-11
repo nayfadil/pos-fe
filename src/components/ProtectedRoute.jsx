6954fc076923d4f91e3d1c64ae46b5676103f2ec
nayfadil <42664410+nayfadil@users.noreply.github.com>
2026-08-11 14:50:49 +0800

    Set isAuthenticated to false in ProtectedRoute

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
 const ProtectedRoute = () => {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated } = false;
 
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
