bfb9a223ef58d3c0f5a4ea8c0c2fa0ee986048ab
nayfadil <42664410+nayfadil@users.noreply.github.com>
2026-08-12 16:08:53 +0800

    feat(agent): add/update src/hooks/useCart.js for Fix ProductGrid Export Mismatch in PosPage

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,10 +1,5 @@
-import { useContext } from 'react';
-import { CartContext } from '../context/CartContext';
+import { useCartContext } from '../context/CartContext';
 
-export const useCart = () => {
-  const context = useContext(CartContext);
-  if (!context) {
-    throw new Error('useCart must be used within a CartProvider');
-  }
-  return context;
-};
+export function useCart() {
+  return useCartContext();
+}
